f2e4568bc2131935f589877c62e8a8c24699ed26
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2024-06-20 14:02:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    VITE_LEVEL: JSON.stringify(process.VITE_LEVEL)
+    VITE_LEVEL: JSON.stringify('LOCAL')
   },
   plugins: [react()],
 })
