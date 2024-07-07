d4e93de6109456f62f11801cbde596be4733fc0f
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2024-07-07 10:25:08 +0300

    Update ErrorBoundary.jsx

--- src/components/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -13,10 +13,6 @@ class ErrorBoundary extends React.Component {
         this.state = { hasError: false };
     }
 
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
-    }
-
     componentDidCatch(error, errorInfo) {
         console.error(error, errorInfo);
         return { hasError: true };
